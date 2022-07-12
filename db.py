6baeb8e3504793770a283435ad0eb1570333fccc
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2022-07-12 02:07:08 -0500

    updates

--- db.py
+++
@@ -1,4 +0,0 @@
-ENDPOINT = "summer.cgikrpj2erfe.us-east-1.rds.amazonaws.com"
-USER = "summersingle"
-PASS = "4kqE{`7Yz.FS;$Cf"
-DB = ""
---
+++ src/Py_Files/CONSTANTS.py
@@ -0,0 +1,8 @@
+HOST = "summer.cgikrpj2erfe.us-east-1.rds.amazonaws.com"
+USER = "summersingle"
+PASSWORD = "4kqE{`7Yz.FS;$Cf"
+DATABASE = "summer"
+
+USER_TABLE = f"{DATABASE}.user_information"
+PROD_TABLE = f"{DATABASE}.product_information"
+KEYS_TABLE = f"{DATABASE}.keys_information"
