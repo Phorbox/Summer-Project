2e8c0afdbaac98139899cd5b8561b5d34121c012
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2022-07-14 02:52:13 -0500

    SQL Queries Start

--- src/Py_Files/Reset_Tables.py
+++ src/Py_Files/Reset_Tables.py
@@ -1,3 +1,4 @@
+from dataclasses import Field, fields
 import mysql.connector
 
 import CONSTANTS
@@ -12,47 +13,54 @@ def Drop_table(Table):
     My_Cursor.execute(sql)
 
 
-UID = "UID        INT             NOT NULL AUTO_INCREMENT"
-USERNAME = "Username   VARCHAR(255)    NOT NULL"
-EMAIL = "Email      VARCHAR(255)    NOT NULL"
-PASSWORD = "Pass       VARCHAR(30)     NOT NULL"
-FIRST = "First_Name VARCHAR(255)    NOT NULL"
-LAST = "Last_Name  VARCHAR(255)    NOT NULL"
-STREET = "Address    VARCHAR(255)    NOT NULL"
-STATE = "State      CHAR(2)         NOT NULL"
-PHONE = "Phone      CHAR(10)        NOT NULL"
-SCORE = "Score      CHAR(2)"
-CART = "Cart         VARCHAR(255)"
-PRIMARY = "PRIMARY KEY (UID)"
+ID = "ID INT NOT NULL AUTO_INCREMENT"
+USER = "USER VARCHAR(255) NOT NULL"
+FIRST = "FIRST VARCHAR(255) NOT NULL"
+LAST = "LAST VARCHAR(255) NOT NULL"
+EMAIL = "EMAIL VARCHAR(255) NOT NULL"
+PASS = "PASS VARCHAR(255) NOT NULL"
+PHONE = "PHONE CHAR(10) NOT NULL"
+CART = "CART VARCHAR(255) NOT NULL"
+ADMINISTRATOR = "ADMINISTRATOR BOOL NOT NULL"
+PRIMARY = "PRIMARY KEY (ID) "
+USERFIELDS = [ID, USER, FIRST, LAST, EMAIL,
+              PASS, PHONE, CART, ADMINISTRATOR, PRIMARY]
 
-USERFIELDS = [UID, USERNAME, EMAIL, PASSWORD, FIRST,
-              LAST, STREET, STATE, PHONE, SCORE, CART, PRIMARY]
+UID = "UID INT NOT NULL"
+NUMBER = "NUMBER INT NOT NULL"
+STREET = "STREET VARCHAR(255) NOT NULL"
+STATE = "STATE CHAR(2) NOT NULL"
+ZIP = "ZIP INT NOT NULL"
+ADDRESSFIELDS = [ID, UID, NUMBER, STREET, STATE, ZIP, PRIMARY]
 
-Pid = "PID int not null auto_increment"
-INAME = "name varchar(256)"
-Price = "price varchar(20)"
-Pic_Id = "picture_id varchar(256)"
-SID = "seller_id int"
-DEsc = "description varchar(256)"
-Quantity = "quantity int"
-Remaining = "remaining_item int"
-catagory = "catagory varchar(256)"
-sub_category = "sub_category varchar(256)"
-tags = "tags varchar(256)"
-PPrimary = "PRIMARY KEY (PID)"
-PRODFIELDS = [Pid, INAME, Price, Pic_Id, SID, DEsc, Quantity,
-              Remaining, catagory, sub_category, tags, PPrimary]
+PID_LIST = "PID_LIST VARCHAR(255) NOT NULL"
+FLAT = "FLAT INT NOT NULL"
+PERCENT = "PERCENT INT NOT NULL"
+START = "START int NOT NULL"
+END = "END int NOT NULL"
+CODE = "CODE VARCHAR(255) NOT NULL"
+DISCOUNTFIELDS = [ID, PID_LIST, FLAT, PERCENT, START, END, CODE, PRIMARY]
 
+UID = "UID INT NOT NULL"
+PID_LIST = "PID_LIST VARCHAR(255) NOT NULL"
+DID_LIST = "DID_LIST VARCHAR(255) NOT NULL"
+TOTAL = "TOTAL INT NOT NULL"
+SALEFIELDS = [ID, UID, PID_LIST, DID_LIST, TOTAL, PRIMARY]
+
+QUANTITY = "QUANTITY INT NOT NULL"
+PRICE = "PRICE INT NOT NULL"
+DESCRIPTION = "DESCRIPTION VARCHAR(255) NOT NULL"
+IMAGE_NAME = "IMAGE_NAME VARCHAR(255) NOT NULL"
+ITEMFIELDS = [ID, QUANTITY, PRICE, DESCRIPTION, IMAGE_NAME, PRIMARY]
 
 START_ID = "ALTER TABLE {} AUTO_INCREMENT=100"
 
+TABLES = [CONSTANTS.USER_TABLE, CONSTANTS.ADDRESS_TABLE,
+          CONSTANTS.DISCOUNT_TABLE, CONSTANTS.SALE_TABLE, CONSTANTS.ITEM_TABLE]
 
-def Make_User_Table():
-    Make_Table(CONSTANTS.USER_TABLE, USERFIELDS)
+FELDS = [USERFIELDS, ADDRESSFIELDS, DISCOUNTFIELDS, SALEFIELDS, ITEMFIELDS]
 
 
-def Make_Prod_Table():
-    Make_Table(CONSTANTS.PROD_TABLE, PRODFIELDS)
 
 
 def Make_Table(userTable, newfields):
@@ -78,16 +86,16 @@ def Format_Statement(list):
     returner = returner[:-len(" , ")]
     return (returner)
 
+
 def Drop_All():
-    Drop_table(CONSTANTS.PROD_TABLE)
-    Drop_table(CONSTANTS.USER_TABLE)
-    Drop_table(CONSTANTS.USER_TABLE)
+    for each in TABLES:
+        Drop_table(each)
+
+
 
 def Make_All():
-    Make_Prod_Table()
-    Make_User_Table()
-
-
+    for each,other in zip(TABLES,FELDS):
+        Make_Table(each,other)
 
 
 Drop_All()
