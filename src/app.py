622745eda16b36ab3c5ab25e26a193c584f352d0
Ethan <dontATme>
2022-07-30 22:53:46 -0500

    adding user issue

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from cgi import print_form
 from flask import Flask, jsonify, request, render_template, send_from_directory, redirect, url_for, session, flash
 from PY_Files.Login_User import Login
 from PY_Files.Items import Get_Items
+from PY_Files.SQL_Queries import Push_To_User_Table
 
 
 app = Flask(__name__)
@@ -42,11 +43,29 @@ def main_menu():
     # lookup item
     if request.method == "POST":
         print("here")
-        print(request.form["search_button"])
+        #print(request.form["search_button"])
     # if request.method == ["POST"] and 'Item_Search' in request.form:
     #     Get_Items
     #     pass
 
+    if request.method == "POST" and 'username' in request.form and 'password' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form:
+        
+        new_user=[request.form['username'], request.form['firstname'], request.form['lastname'], request.form['email'], request.form['password'], 1234567890, 0]
+        print(request.form['username'])
+        print(new_user)
+        
+        if request.form['username'] == "":
+            print("nothing there")
+            
+        if request.form['username'] != "":
+            print("something there")
+            Push_To_User_Table(new_user)
+            
+            
+        print("add user request recieved")
+        
+    if request.method == "POST":
+        print("add discount request recieved")
 
     return render_template('administration_interface/foc_admin_interface_menu.html')
 
