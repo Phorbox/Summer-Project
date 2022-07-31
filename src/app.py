b482a13c21f384ae739e0634bb4b2881817e2d5f
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2022-07-31 11:42:24 -0500

    cleaned up app.py

--- src/app.py
+++ src/app.py
@@ -18,54 +18,56 @@ def login():
         return redirect(url_for('main_menu'))
 
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        Credentials=[request.form['username'],request.form['password']]
+        Credentials = [request.form['username'], request.form['password']]
         account = Login(Credentials)
-        
+
         if account == None:
-                flash('Incorrect User information')
-                return render_template('administration_interface/foc_admin_interface_login.html')
+            flash('Incorrect User information')
+            return render_template('administration_interface/foc_admin_interface_login.html')
 
         if account[1] == 1:
             session["user"] = account[0]
             session["admin"] = account[1]
             return redirect(url_for('main_menu'))
 
-        
     return render_template('administration_interface/foc_admin_interface_login.html')
+
 
 @app.route("/main_menu", methods=['GET', 'POST'])
 def main_menu():
     if not("user" in session):
         return redirect(url_for('login'))
+
     if session.get("admin"):
         pass
 
-    # lookup item
-    if request.method == "POST":
-        print("here")
-        #print(request.form["search_button"])
-    # if request.method == ["POST"] and 'Item_Search' in request.form:
-    #     Get_Items
-    #     pass
-
     if request.method == "POST" and 'username' in request.form and 'password' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form:
-        
-        new_user=[request.form['username'], request.form['firstname'], request.form['lastname'], request.form['email'], request.form['password'], 1234567890, 0]
+
+        new_user = [request.form['username'], request.form['firstname'], request.form['lastname'],
+                    request.form['email'], request.form['password'], 1234567890, "", 0]
         print(request.form['username'])
         print(new_user)
-        
+
         if request.form['username'] == "":
             print("nothing there")
-            
+
         if request.form['username'] != "":
             print("something there")
             Push_To_User_Table(new_user)
-            
-            
+
         print("add user request recieved")
-        
-    if request.method == "POST":
-        print("add discount request recieved")
+
+    if request.method == "POST" and request.form.get("search_button") == "SEARCH E-MAILS":
+        me = 0
+
+    if request.method == "POST" and request.form.get("search_button") == "SEARCH ITEMS":
+        me = 0
+
+    if request.method == "POST" and request.form.get("search_button") == "SEARCH ORDERS":
+        me = 0
+
+    if request.method == "POST" and request.form.get("search_button") == "SEARCH USERS":
+        me = 0
 
     return render_template('administration_interface/foc_admin_interface_menu.html')
 
@@ -76,4 +78,4 @@ def logout():
     return redirect(url_for('login'))
 
 
-app.run(debug = True)
+app.run(debug=True)
