26d9d084ed30955f61d237e27f9998d104a0c5a6
Ethan <dontATme>
2022-07-31 01:45:27 -0500

    finished add menu [[[[ ? ]]]]

--- src/app.py
+++ src/app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request, render_template, send_from_directory,
 from PY_Files.Login_User import Login
 from PY_Files.Items import Get_Items
 from PY_Files.SQL_Queries import Push_To_User_Table
+from PY_Files.SQL_Queries import Push_To_DISCOUNT_Table
+from PY_Files.SQL_Queries import Push_To_ITEM_Table
 
 
 app = Flask(__name__)
@@ -48,24 +50,24 @@ def main_menu():
     #     Get_Items
     #     pass
 
-    if request.method == "POST" and 'username' in request.form and 'password' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form:
+    if request.method == "POST" and 'username' in request.form and 'password' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form and 'phone' in request.form:
         
-        new_user=[request.form['username'], request.form['firstname'], request.form['lastname'], request.form['email'], request.form['password'], 1234567890, 0]
-        print(request.form['username'])
-        print(new_user)
-        
-        if request.form['username'] == "":
-            print("nothing there")
-            
-        if request.form['username'] != "":
-            print("something there")
-            Push_To_User_Table(new_user)
-            
-            
+        new_user=[request.form['username'], request.form['firstname'], request.form['lastname'], request.form['email'], request.form['password'], request.form['phone'], "", 0]
+        Push_To_User_Table(new_user)
         print("add user request recieved")
         
-    if request.method == "POST":
+    if request.method == "POST" and 'code' in request.form and 'percent-off' in request.form and 'discount-id' in request.form and 'start-date' in request.form and 'end-date' in request.form and 'flat' in request.form:
+        
+        new_discount=[request.form['discount-id'], request.form['flat'], request.form['percent-off'], request.form['start-date'], request.form['end-date'], request.form['code'] ]
+        Push_To_DISCOUNT_Table(new_discount)
         print("add discount request recieved")
+        
+    if request.method == "POST" and 'item-name' in request.form and 'quantity' in request.form and 'item-description' in request.form and 'item-id' in request.form and image-name in request.form:
+        
+        new_item=[request.form['item-name'], request.form['item-id'], request.form['quantity'], request.form['item-description'], request.form['image-name']]
+        Push_To_ITEM_Table(new_item)
+        print("add item request recieved")
+    
 
     return render_template('administration_interface/foc_admin_interface_menu.html')
 
