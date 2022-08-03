a1097d06ce8b18bb8fa0faa8c1a70ebbf8f2f18c
Matthew Henderson <48498205+Phorbox@users.noreply.github.com>
2022-08-03 00:36:12 -0500

    Final

--- src/app.py
+++ src/app.py
@@ -129,5 +129,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080) 
-    app.run(debug=True) 
+    app.run(host='0.0.0.0', port=8080) 
+    # app.run(debug=True) 
